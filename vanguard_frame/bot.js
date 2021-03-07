4a9555ea3c370950888d3a419b5cc4eb989b2a3d
LEPT0N <dan.mitchell@outlook.com>
2021-03-07 10:18:28 -0800

    adding gilded_seals to all_leaderboards

--- vanguard_frame/bot.js
+++ vanguard_frame/bot.js
@@ -134,6 +134,7 @@ async function print_all_leaderboards(input)
         { arguments: ['triumphs', 'exotic_catalysts'] },
         { arguments: ['triumphs', 'lore'] },
         { arguments: ['triumphs', 'seals'] },
+        { arguments: ['triumphs', 'gilded_seals'] },
         { arguments: ['triumphs', 'seasonal_challenges'] },
 
         { arguments: ['individual_stat', 'killing_spree'] },
