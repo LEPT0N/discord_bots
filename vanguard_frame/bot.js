01a624a05b148f53b0685dd19dcbbb1e49e9a6de
LEPT0N <dan.mitchell@outlook.com>
2021-08-16 02:47:53 -0700

    removing useless gilded-seal-leaderboards from 'all'

--- vanguard_frame/bot.js
+++ vanguard_frame/bot.js
@@ -132,11 +132,8 @@ async function print_all_leaderboards(input)
         { arguments: ['triumphs', 'exotic_catalysts'] },
         { arguments: ['triumphs', 'lore'] },
         { arguments: ['triumphs', 'seals'] },
-        { arguments: ['triumphs', 'gilded_seals'] },
-        { arguments: ['metrics', 'unbroken_gild_count'] },
         { arguments: ['metrics', 'dredgen_gild_count'] },
         { arguments: ['metrics', 'conqueror_gild_count'] },
-        { arguments: ['metrics', 'flawless_gild_count'] },
         { arguments: ['triumphs', 'seasonal_challenges'] },
 
         { arguments: ['individual_stat', 'killing_spree'] },
